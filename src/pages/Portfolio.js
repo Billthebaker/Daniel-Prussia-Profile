ff01969e2ba2c16debf14d3733c09d10c52f1e73
unknown <dbp1795@aim.com>
2023-04-20 01:20:38 -0400

    got boxes for portfolio to show, will add styling tomorrow

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import './index.css'
+import './index.css';
+import image1 from '../images/THATSME.jpg';
+import image2 from '../images/cartoonify.png';
+import image3 from '../images/lightbulbs.jpg';
+import image4 from '../images/sky.jpg';
+import image5 from '../images/spacebackground.jpg';
+import image6 from '../images/forest.jpg';
 
 function Portfolio() {
+  // Define the image URLs for each box
+  const img1Url = `url(${image1})`;
+  const img2Url = `url(${image2})`;
+  const img3Url = `url(${image3})`;
+  const img4Url = `url(${image4})`;
+  const img5Url = `url(${image5})`;
+  const img6Url = `url(${image6})`;
+
   return (
     <div className="Portfolio">
       <header className="Portfolio-header">
         <h2>My Portfolio</h2>
       </header>
       <section className="Portfolio-content">
-        <p>Here's some content for my portfolio page.</p>
+        <div className="box-container">
+          <div className="box" style={{ backgroundImage: img1Url }}>
+            <p>Box 1</p>
+          </div>
+          <div className="box" style={{ backgroundImage: img2Url }}>
+            <p>Box 2</p>
+          </div>
+          <div className="box" style={{ backgroundImage: img3Url }}>
+            <p>Box 3</p>
+          </div>
+          <div className="box" style={{ backgroundImage: img4Url }}>
+            <p>Box 4</p>
+          </div>
+          <div className="box" style={{ backgroundImage: img5Url }}>
+            <p>Box 5</p>
+          </div>
+          <div className="box" style={{ backgroundImage: img6Url }}>
+            <p>Box 6</p>
+          </div>
+        </div>
       </section>
       <footer className="Portfolio-footer">
         <p>Thanks for visiting my portfolio!</p>
